cdc398207d52678772fb38f0680a9e82576e82b0
roger <s.cherednichenko@vex-mobile.com>
2021-06-09 19:55:55 +0800

    Profile && Signout

--- presentation/src/main/java/ru/vsibi/presentation/screens/profile/ProfileFragment.kt
+++ presentation/src/main/java/ru/vsibi/presentation/screens/profile/ProfileFragment.kt
@@ -1,8 +1,67 @@
 package ru.vsibi.presentation.screens.profile
 
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.viewModels
+import com.google.android.material.snackbar.Snackbar
+import dagger.hilt.android.AndroidEntryPoint
 import ru.vsibi.presentation.R
 import ru.vsibi.presentation.base.BaseFragment
 import ru.vsibi.presentation.databinding.FragmentHotelsBinding
+import ru.vsibi.presentation.databinding.FragmentProfileBinding
+import ru.vsibi.presentation.screens.search.main.SearchAction
+import ru.vsibi.presentation.screens.search.main.SearchViewState
 
-class ProfileFragment : BaseFragment<FragmentHotelsBinding>(FragmentHotelsBinding::inflate, R.layout.fragment_hotels) {
+@AndroidEntryPoint
+class ProfileFragment :
+    BaseFragment<FragmentProfileBinding>(FragmentProfileBinding::inflate, R.layout.fragment_profile) {
+
+    private val viewModel: ProfileViewModel by viewModels()
+
+    override fun initViews() {
+        (activity as AppCompatActivity).supportActionBar?.apply {
+            title = getString(R.string.profile)
+            setDisplayHomeAsUpEnabled(false)
+            setDisplayShowHomeEnabled(false)
+        }
+    }
+
+    override fun initArguments() {
+        super.initArguments()
+    }
+
+    override fun initFragment() {
+        super.initFragment()
+    }
+
+    override fun initListeners() {
+        binding.apply {
+            linSignout.setOnClickListener {
+                viewModel.obtainEvent(ProfileEvent.LogOut())
+            }
+        }
+    }
+
+    override fun initData() {
+        super.initData()
+    }
+
+    override fun initObservers() {
+        viewModel.viewStates().observe(viewLifecycleOwner) { bindViewState(it) }
+        viewModel.viewActions().observe(viewLifecycleOwner) { bindViewActions(it) }
+    }
+
+    private fun bindViewState(state: ProfileViewState) {
+        when (state) {
+            is ProfileViewState.LoggedOut -> router.reopenApp()
+        }
+    }
+
+    private fun bindViewActions(action: ProfileAction?) {
+        when (action) {
+            is ProfileAction.ShowSnackBar -> Snackbar.make(binding.root, getString(action.data), Snackbar.LENGTH_LONG)
+                .show()
+        }
+    }
+
 }
